7747c5d19603cf5a1d7edb5be658f3f6e76479e0
rgslagle <rgslagle@fullsail.edu>
2014-06-10 19:56:46 -0500

    Expressions Worksheet 1
    First example completed

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,12 @@
+//Ryan Slagle; June 10, 2014; Expressions Worksheet
+
+//Calculate the area of a rectangle given the width and height of the rectangle.
+
+var height = 3;  //create a variable for the height
+var width = 4; //create a variable for the width
+
+var area = height * width; //create a variable for the height times width for area
+
+console.log(area); //print out result in console
+
+
