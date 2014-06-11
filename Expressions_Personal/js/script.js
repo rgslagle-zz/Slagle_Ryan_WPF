6dec3e57fe16ebe08b66184801dc7480e12170cb
rgslagle <rgslagle@fullsail.edu>
2014-06-11 18:52:05 -0500

    Expressions Personal
    Ensuring the math aligns with my formulas

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,14 +1,17 @@
 //Ryan Slagle; June 10, 2014; Expressions_Personal
 
 
-var golfBalls = prompt("How many golf balls do you have in your bag?");
+var golfRound = 18;
 
 var golfHoles = prompt("How many holes of golf are you playing?");
 
-var ballsNeeded = prompt ("How many balls do you loose in a round?")
+var ballsUsed = prompt("How many balls do you loose in a round?");
 
 
+var ballsNeeded = golfHoles / golfRound * ballsUsed;
 
+
+console.log(ballsNeeded)
 
 
 
