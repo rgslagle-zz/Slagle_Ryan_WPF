b278c3a5374592b5cce171fe5d50045c7a8648c7
rgslagle <rgslagle@fullsail.edu>
2014-06-11 18:45:27 -0500

    Expressions Personal
    Started to build formula for golf balls needed for a round of golf.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,14 @@
 //Ryan Slagle; June 10, 2014; Expressions_Personal
 
 
+var golfBalls = prompt("How many golf balls do you have in your bag?");
+
+var golfHoles = prompt("How many holes of golf are you playing?");
+
+var ballsNeeded = prompt ("How many balls do you loose in a round?")
+
+
+
+
+
+
