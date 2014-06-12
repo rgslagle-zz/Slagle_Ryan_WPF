e28f1e5fa86ee36ffd614ff9164b012e695aa290
rgslagle <rgslagle@fullsail.edu>
2014-06-12 17:51:58 -0500

    Confused Changes
    Made changes based on working through knowledge issues

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,12 @@
-//Ryan Slagle; June 10,2014; Expressions_Industry
+/*Ryan Slagle
+June 10,2014
+Expressions_Industry
+ */
+
+//Create variables that are relevant to my industry
+// Number of pages on a website needed
+
+var numPages = [5, 10, 15, 20];
+
+
+var webSite = numPages
--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,9 +1,3 @@
 //Ryan Slagle; June 10, 2014; Expressions_Wacky
 
 
-var empSales = [100, 200, 300, 400, 500]; //Sales Reported by Staff
-
-var totalSales = empSales[0] + empSales[1] + empSales[2] + empSales[3] + empSales[4]; //Total Sales Reported
-
-
-console.log(totalSales);
