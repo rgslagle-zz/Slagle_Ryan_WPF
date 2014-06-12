f43322e816ac12b658178a027be1c57bff0c2a61
rgslagle <rgslagle@fullsail.edu>
2014-06-11 21:11:10 -0500

    Expressions Wacky
    Working on Arrays

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,14 +1,14 @@
 //Ryan Slagle; June 10, 2014; Expressions_Personal
 
 
-var golfRound = 18;
+var golfRound = 18; //Allows for more than 1 round for calculations (constant)
 
-var golfHoles = prompt("How many holes of golf are you playing?");
+var golfHoles = prompt("How many holes of golf are you playing?");//First Variable
 
-var ballsUsed = prompt("How many balls do you loose in a round?");
+var ballsUsed = prompt("How many balls do you loose in a round?");//Second Variable
 
 
-var ballsNeeded = prompt(golfHoles / golfRound * ballsUsed + 1);
+var ballsNeeded = golfHoles / golfRound * ballsUsed + 1;// Adding 1 so you can finish the round!
 
 
 console.log(ballsNeeded)
--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,11 @@
 //Ryan Slagle; June 10, 2014; Expressions_Wacky
+
+
+var empNames = ["Kurtis", "Monica", "Damita", "Nikita", "Malissa"];
+
+var salesToday = ["100", "200", "300", "400", "500"];
+
+var totalSales += empNames
+
+var dailyResults += empNames[0]
+
