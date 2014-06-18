a3b3be6d2237d6e2c9662bddea4ae979bcff9c3a
rgslagle <rgslagle@fullsail.edu>
2014-06-17 22:16:58 -0500

    Conditional Personal
    Really Getting There

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -9,13 +9,16 @@ Conditionals Personal
 //If the hole is over 250 yards hit the driver...If not hit else.
 
 var holeLength = prompt("How many yards is the hole?");
+var normalHit = prompt("How far do you hit your Wedge?");
 
+var hitDriver = (holeLength - normalHit);
 
 
 //creating conditional logic based on user generated information.
-if(hitDriver){
-    //code performed if condition is greater than
-       console.log(hitDriver);
+if(hitDriver > 250){
+    //If hitDriver var is greater than 250 than hit your driver.
+    alert("With only hitting your wedge " +  normalHit + " you have " + hitDriver + " yards you need to get there.  Hit your driver!");
+    console.log("With only hitting your wedge " +  normalHit + " you have " + hitDriver + " yards you need to get there.  Hit your driver!");
 }
 
 
