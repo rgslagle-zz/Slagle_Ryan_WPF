3decf897b6437e742864059d866b74e9a1e1e1bd
rgslagle <rgslagle@fullsail.edu>
2014-06-17 20:30:58 -0500

    Conditionals Personal
    Outlining a scenario for my variables

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -6,3 +6,9 @@ Conditionals Personal
 //Create conditionals for something personal to me.
 //This will be based on checking variables to see if I am in a hazard or not in golf
 
+//Creating variables for type of lie my ball was hit in.
+
+var fairway = True;
+var rough = True;
+var sand = false;
+
