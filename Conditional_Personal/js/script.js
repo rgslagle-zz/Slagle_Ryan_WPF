a17fe70f3857f91e2cf986b55198079fba200bf2
rgslagle <rgslagle@fullsail.edu>
2014-06-17 20:50:07 -0500

    Conditional Personal
    Changed title, and noted some key changes for prompts, alerts, and user
    input

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -7,8 +7,19 @@ Conditionals Personal
 //This will be based on checking variables to see if I am in a hazard or not in golf
 
 //Creating variables for type of lie my ball was hit in.
+//if the ball was hit in the fairway print to the console, "Great Job!"
 
-var fairway = True;
-var rough = True;
-var sand = false;
+var fairway = prompt("Did you hit the ball in the fairway?");
+var greatJob = (fairway);
+
+if(fairway){
+    alert("Great Job!");
+    console.log("Great Job!");
+}
+
+
+
+
+
+
 
