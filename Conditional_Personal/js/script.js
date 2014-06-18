b21a7c03c71bd8010550fb6f34a75e0e736d6695
rgslagle <rgslagle@fullsail.edu>
2014-06-17 20:28:02 -0500

    Conditional Folders Created
    created all of the folders needed for assignment upload

---
+++ Conditional_Personal/js/script.js
@@ -0,0 +1,8 @@
+/* Ryan Slagle
+June 17, 2014
+Conditionals Personal
+ */
+
+//Create conditionals for something personal to me.
+//This will be based on checking variables to see if I am in a hazard or not in golf
+
