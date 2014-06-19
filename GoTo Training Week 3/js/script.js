54d10074078665c416f30c882a34285338aeedab
rgslagle <rgslagle@fullsail.edu>
2014-06-19 18:46:58 -0500

    Setup First Conditional
    Tested if first fruit was pear

--- GoTo Training Week 3/js/script.js
+++ GoTo Training Week 3/js/script.js
@@ -15,14 +15,27 @@ var bowlOfFruit = ["Apple", "Banana", "Pear", "Peach", "Pear"];
 console.log(bowlOfFruit);
 
 //Access individual item with index number
+//Create var that will track how many pears we have in total
+var totalNumPears = 0;
 
 console.log(bowlOfFruit[1]);
 
 //length of the Array
 console.log(bowlOfFruit.length);
 
+//Counting how many pears are in the bowl?
 
+//Test each fruit and see if it is a pear
 
+if(bowlOfFruit[0]==="Pear"){
+
+    //count that fruit as a Pear
+    //add 1 to the variable totalNumPears
+    totalNumPears++;
+}else{
+    console.log("Fruit is not a Pear");
+
+}
 
 
 
