67b4b2020d04b3295e4f1003d7860923dd28f240
rgslagle <rgslagle@fullsail.edu>
2014-06-19 18:38:11 -0500

    GoTo Training Week 3
    length of array

--- GoTo Training Week 3/js/script.js
+++ GoTo Training Week 3/js/script.js
@@ -5,6 +5,25 @@ GoTo Training Week 3
 */
 
 //alert("testing 1,2,3");
+//Code to check how many pears are in a fruit bowl
+
+//Create our "fruit bowl"
+//Create an Array
+
+var bowlOfFruit = ["Apple", "Banana", "Pear", "Peach", "Pear"];
+
+console.log(bowlOfFruit);
+
+//Access individual item with index number
+
+console.log(bowlOfFruit[1]);
+
+//length of the Array
+console.log(bowlOfFruit.length);
+
+
+
+
 
 
 
