350451747cebd1f2be4a769df8a7a2bd94baf062
rgslagle <rgslagle@fullsail.edu>
2014-06-19 19:48:07 -0500

    Validated Prompts Code
    Code for validating prompts

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,12 +7,25 @@ Conditional Industry
 //Creating conditional var for how many sales a company needs to buy supplies
 
 var supplies = prompt("How much does your order of supplies cost?");
+
+if(supplies===""){
+    supplies =prompt("Please tell us in dollars how much your supplies \nyou want cost!")
+}
 var eachSale = prompt("What is your average sale?");
+
+if(eachSale===""){
+    eachSale = prompt("Again how much is your average sale?")
+}
 var expenses = prompt("How much do you have in other expenses?");
+
+if(expenses===""){
+    expenses = prompt("Please enter your monthly expenses.  \nBills Need Paid!")
+
+}
 //create conditionals to determine if sales allow for supplies purchase
 if(eachSale > supplies && expenses< 1000){
     //Print out to the console, and give the user an alert
-    console.log("You can buy supplies");
+    console.log("You can buy supplies!");
     alert("You can buy your supplies!")
 }else{
     console.log("You need to make more sales!")
