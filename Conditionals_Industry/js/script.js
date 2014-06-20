0fb78dd246762a85b62ac651b260a9779d53d572
rgslagle <rgslagle@fullsail.edu>
2014-06-19 19:26:22 -0500

    Conditionals Industry
    Created Variables and IfElse

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,18 @@
+/*
+Ryan Slagle
+June 19, 2014
+Conditional Industry
+ */
 
+//Creating conditional var for how many sales a company needs to buy supplies
+
+var supplies = prompt("How many dollars to you spend on supplies?");
+var eachSale = prompt("What is your average sale?");
+var expenses = prmopt("How much do you have in other expenses?");
+
+if(){
+
+
+}else{
+    
+}
