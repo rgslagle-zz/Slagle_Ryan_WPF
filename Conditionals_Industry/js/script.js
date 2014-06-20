5be791e1ce29ae1294c8cd595e75e0521d2f8672
rgslagle <rgslagle@fullsail.edu>
2014-06-19 19:36:32 -0500

    Notes on lines
    put notes in on what I was trying to do

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,10 +9,12 @@ Conditional Industry
 var supplies = prompt("How much does your order of supplies cost?");
 var eachSale = prompt("What is your average sale?");
 var expenses = prompt("How much do you have in other expenses?");
-
+//create conditionals to determine if sales allow for supplies purchase
 if(eachSale > supplies && expenses< 1000){
+    //Print out to the console, and give the user an alert
     console.log("You can buy supplies");
-
+    alert("You can buy your supplies!")
 }else{
     console.log("You need to make more sales!")
+    alert("You need to make more sales!")
 }
