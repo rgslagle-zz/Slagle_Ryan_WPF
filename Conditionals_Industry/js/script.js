9fdf96a038e5e447bfc65d551480cdb6fe4615d9
rgslagle <rgslagle@fullsail.edu>
2014-06-19 19:32:10 -0500

    If Else Conditions
    If Else Conditions

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,13 +6,13 @@ Conditional Industry
 
 //Creating conditional var for how many sales a company needs to buy supplies
 
-var supplies = prompt("How many dollars to you spend on supplies?");
+var supplies = prompt("How much does your order of supplies cost?");
 var eachSale = prompt("What is your average sale?");
-var expenses = prmopt("How much do you have in other expenses?");
+var expenses = prompt("How much do you have in other expenses?");
 
-if(){
-
+if(eachSale > supplies && expenses< 1000){
+    console.log("You can buy supplies");
 
 }else{
-    
+    console.log("You need to make more sales!")
 }
