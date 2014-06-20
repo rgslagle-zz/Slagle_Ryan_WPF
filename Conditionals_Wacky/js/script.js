17465562e03686cb3f8ff2df5c507acbabb08dff
rgslagle <rgslagle@fullsail.edu>
2014-06-19 21:28:29 -0500

    Conditionals Wacky
    Not so Wacky with conditionals here

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,28 @@
+/*
+Ryan Slagle
+June 19, 2014
+Conditional Wacky
+ */
+//Going to a baseball game and need to know what to take.
+
+var temp = prompt("What is the temperature outside?");
+
+if(temp===""){
+    temp = prompt("What is the temperature outside?");
+
+}
+var hat;
+//Ternary operator
+hat = (temp >= 80) ? "Wear sunscreen, a hat, and sunglasses" : "Not a perfect day, but play ball!";
+alert(hat);
+console.log(hat);
+}
+
+
+
+
+
+
+
+
 
