b106a6be2f29f317264579fcfb20bf83836ccbf3
rgslagle <rgslagle@fullsail.edu>
2014-06-25 10:30:32 -0500

    Comments
    commented out assignment requirements

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,11 @@ Ryan Slagle
 June 25 2014
 Functions Wacky
  */
+
+//A ternary
+//An else if
+//An anonymous function
+//A normal "named" function
+//An expression with two arithmetic operators
+//A function with three parameters.
+//At least one logical operator
