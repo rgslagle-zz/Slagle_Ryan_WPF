b31c8e60dde92c7f29052ac46c072070b842b135
rgslagle <rgslagle@fullsail.edu>
2014-06-25 10:31:42 -0500

    Comments
    Commented out assignment requirements

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,3 +4,10 @@ June 25 2014
 Functions Industry
  */
 
+//A ternary
+//An else if
+//An anonymous function
+//A normal "named" function
+//An expression with two arithmetic operators
+//A function with three parameters.
+//At least one logical operator
