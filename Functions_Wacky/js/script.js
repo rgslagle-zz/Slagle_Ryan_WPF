ad9b85c27293ee3bb83427fcb6b2bcf700eb1366
rgslagle <rgslagle@fullsail.edu>
2014-06-26 12:00:54 -0500

    Comments Added
    Added some comments to code

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -4,20 +4,15 @@ June 25 2014
 Functions Wacky
  */
 
-//A ternary
-//An else if
-//An anonymous function
-//A normal "named" function
-//An expression with two arithmetic operators
-//A function with three parameters.
-//At least one logical operation
-
+//Letting the user know if they had a good season or not based on wins and losses based on how many games they played.
 var winningGames = prompt("How many wins does your team have?");
-
+//validated
 while (isNaN(winningGames)|| winningGames==="") {
     winningGames = prompt("Please type in a minimum number.");
 }
+//variable
 var lossingGames = prompt("How many losses does your team have?");
+//validated
 while (isNaN(lossingGames)|| lossingGames==="") {
     lossingGames = prompt("Please type in a minimum number.");
 }
@@ -26,9 +21,9 @@ function totalGames(){//Creating a basic function
     var wins = winningGames;//variables within the function
     var lose = lossingGames;
     var gamesPlayed = Number(wins) + Number(lose);
-    console.log(gamesPlayed);
 }
 
 totalGames(Number(winningGames + lossingGames));//Invoking the function
 
+//Ternary Operator
 (winningGames > lossingGames) ? alert("Great Season!") : alert("Better Luck Next Year!")
