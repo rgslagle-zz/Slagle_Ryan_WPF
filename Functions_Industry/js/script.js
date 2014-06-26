f9260591464713fbc36dad30b2bc5e3c0c2ed94e
rgslagle <rgslagle@fullsail.edu>
2014-06-26 09:32:57 -0500

    GoToTraining Week 4
    GoToTraining Week 4

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,9 +9,36 @@ Functions Industry
 //An anonymous function
 //A normal "named" function
 //An expression with two arithmetic operators
-//A function with three parameters.
+//A function with three parameters
 //At least one logical operator
 
-function functionName(){
+//Get numbers from the user
 
+var minNumber = prompt("Please type in a minimum number.");
+//validate
+while (isNaN(minNumber)|| minNumber===""){
+    minNumber = prompt("Please type in a minimum number.");
 }
+
+var maxNumber = prompt("Please type in a max number.");
+//validate
+while (isNaN(maxNumber)|| maxNumber===""){
+    minNumber = prompt("Please type in a maximum number.");
+}
+
+//Call Function
+//With Arguments
+//randomNumber(20,50);
+//randomNumber(0, 5);
+
+randomNumber(minNumber, maxNumber);
+
+
+//Create a function
+
+function randomNumber(minNum, maxNum){
+    //generate random number
+    var random = Math.random()*(maxNum-minNum)+Number(minNumber);
+    console.log(random);
+}
+
--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -10,10 +10,19 @@ Functions Wacky
 //A normal "named" function
 //An expression with two arithmetic operators
 //A function with three parameters.
-//At least one logical operator
+//At least one logical operation
 
-var functionName = function(){
-
+var winningGames = prompt("How many wins does your team have?");
+var lossingGames = prompt("How many losses does your team have?");
+function totalGames(){//Creating a basic function
+    var wins = winningGames;//variables within the function
+    var losses = lossingGames;
+    var gamesPlayed = wins + losses;
+    console.log(gamesPlayed);
 }
 
-functionName();
+while (isNaN(minNumber)|| minNumber===""){
+    minNumber = prompt("Please type in a minimum number.");
+}
+
+totalGames();//Invoking the function
