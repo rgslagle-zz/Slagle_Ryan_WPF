d8025a9a369a8e7eb6a24c97642b98c0eb03d5ba
rgslagle <rgslagle@fullsail.edu>
2014-06-26 12:04:27 -0500

    More comments
    Preparing for Turn In

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,15 +4,6 @@ June 25 2014
 Functions Personal
  */
 
-//A ternary
-//An else if
-//An anonymous function
-//A normal "named" function
-//An expression with two arithmetic operators
-//A function with three parameters.
-//At least one logical operator
-
-
 //create variable to determine time of travel for a given distance
 var speed = prompt("How fast will you be driving in MPH?");
 
@@ -28,12 +19,13 @@ var distance = prompt("How many miles are you going?");
 while (isNaN(distance)|| distance==="") {
     distance = prompt("Your speed of travel is needed in order to\ndetermine trip time.\nPlease enter how fast you will be traveling in MPH");
 }
-
+//Anonymous Functions
 var driveTime = function(distance, speed){
     var travel = distance/speed * 60;
     return(travel);
 }
-
+//Invoking the function after the defining it.
 var drive = driveTime(distance, speed);
 console.log(drive);
+//creating an alert for the user.
 alert("Your drive time will be " + Number(drive)+ " minutes");
